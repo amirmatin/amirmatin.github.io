0a6714b6e7fe447c7e8016b40a7bab210bd967d7
DESKTOP-EQM2K8K\amira <amiralimtn@gmail.com>
2017-07-14 11:33:39 +0100

    c

--- php/contact_me.php
+++ php/contact_me.php
@@ -2,8 +2,8 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "amiralimtn@gmail.com";
-    $email_subject = "Subject";
+    $email_to = "you@yourdomain.com";
+    $email_subject = "Your email subject line";
  
     function died($error) {
         // your error code can go here
@@ -16,16 +16,21 @@ if(isset($_POST['email'])) {
  
  
     // validation expected data exists
-    if(!isset($_POST['name']) ||
+    if(!isset($_POST['first_name']) ||
+        !isset($_POST['last_name']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['subject'])) {
+        !isset($_POST['telephone']) ||
+        !isset($_POST['comments'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
  
      
-    $first_name = $_POST['name']; // required
+ 
+    $first_name = $_POST['first_name']; // required
+    $last_name = $_POST['last_name']; // required
     $email_from = $_POST['email']; // required
-    $comments = $_POST['subject']; // required
+    $telephone = $_POST['telephone']; // not required
+    $comments = $_POST['comments']; // required
  
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -40,6 +45,9 @@ if(isset($_POST['email'])) {
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
   }
  
+  if(!preg_match($string_exp,$last_name)) {
+    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+  }
  
   if(strlen($comments) < 2) {
     $error_message .= 'The Comments you entered do not appear to be valid.<br />';
@@ -60,7 +68,9 @@ if(isset($_POST['email'])) {
      
  
     $email_message .= "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
     $email_message .= "Comments: ".clean_string($comments)."\n";
  
 // create email headers
